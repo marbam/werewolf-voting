5d5319704bcfbb44f55a4cc62585c29be7d92aa3
Martin Bampton <martinbampton@hotmail.com>
2020-06-25 15:19:17 +0100

    Saving games and players during Setup phase

--- app/Http/Controllers/SetupController.php
+++ app/Http/Controllers/SetupController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Role;
+use App\Game;
+use App\Player;
+use Carbon\Carbon;
+
 class SetupController extends Controller
 {
     public function getRoles()
@@ -13,8 +17,28 @@ class SetupController extends Controller
                    ->get(['roles.id', 'roles.name'])->toArray();
     }
 
-    public function savePlayers()
+    public function savePlayers(Request $request)
     {
-        // to be done when we've got the database ready.
+        $date = Carbon::now()->isoFormat('DDmmYY');
+        $random = rand(0, 1000);
+        $code = $date.$random;
+
+        $game = Game::create([
+            'code' => $code, // bodging these in for now, it's historical data but no harm in keeping it.
+            'moderator_id' => 1 // can look at this at a later date, use '1' for now.
+        ]);
+
+        $playerData = $request->all()[0];
+
+        foreach ($playerData as $index => $data) {
+            Player::create([
+                'name' => $data['name'],
+                'game_id' => $game->id,
+                'allocated_role_id' => $data['roleId'],
+                'listing_order' => $index+1
+            ]);
+        }
+
+        return ['game_id' => $game->id];
     }
 }
