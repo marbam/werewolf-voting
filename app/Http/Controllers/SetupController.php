ed52bddc4ddadd5a26f9b45e7e4b050c5d0b3cfb
Martin Bampton <martinbampton@hotmail.com>
2020-06-25 14:42:18 +0100

    Hooking up loading of roles for Setup Component

--- app/Http/Controllers/SetupController.php
+++ app/Http/Controllers/SetupController.php
@@ -3,22 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Role;
 class SetupController extends Controller
 {
     public function getRoles()
     {
-        // plug this in later
-        return [
-            ['id' => 1, 'name' => 'Clairvoyant'],
-            ['id' => 2, 'name' => 'Witch'],
-            ['id' => 3, 'name' => 'Wizard'],
-            ['id' => 4, 'name' => 'Healer'],
-            ['id' => 5, 'name' => 'Alpha'],
-            ['id' => 6, 'name' => 'Pack'],
-            ['id' => 7, 'name' => 'Pup'],
-            ['id' => 8, 'name' => 'Defector'],
-        ];
+        return Role::join('factions', 'roles.faction_id', '=', 'factions.id')
+                   ->where('factions.moons', 1)
+                   ->get(['roles.id', 'roles.name'])->toArray();
     }
 
     public function savePlayers()
