0c6a35e08757c136b688841f74227f874ec3e92f
Martin Bampton <martinbampton@hotmail.com>
2020-06-30 16:46:19 +0100

    Logic to generate the ballot results

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Route::get('/', function () {
 
 Route::get('/game/{id}', 'ModController@showGame');
 Route::get('/game/{id}/{type}/{voteId}/', 'PlayerController@showNominees');
+Route::get('/test_ballot/{id}/{voteId}/', 'ModController@getAccusationResults');
