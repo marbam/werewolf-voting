67db4362f940360ec588b8171156636bffa24c72
Martin Bampton <martinbampton@hotmail.com>
2020-07-04 18:56:43 +0100

    Reordering API Routing code for readability.

--- routes/api.php
+++ routes/api.php
@@ -14,26 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
+// Initial Setup Routing:
 Route::get('/get_roles', 'SetupController@getRoles');
 Route::post('/save_players', 'SetupController@savePlayers');
+
+// Routes for Moderator View
 Route::get('/get_players/{id}', 'ModController@getPlayers');
 Route::get('/change_alive_status/{player_id}', 'ModController@changeAliveStatus');
-Route::get('/get_accusable/{game}/{round}', 'PlayerController@getAccusable');
-
-// Route::get('/generate_accusations/{game_id}', 'ModController@createAccusations');
 Route::get('/new_accusations/{game_id}', 'ModController@newAccusations');
 Route::get('/refresh_accusations/{game_id}/{round}', 'ModController@refreshAccusations');
 Route::get('/recall_accusations/{gameId}', 'ModController@recallAccusations');
-
-Route::post('/submit_action/{game_id}/{round_id}', 'PlayerController@submitAction');
 Route::get('/get_accusation_totals/{gameId}/{roundId}/', 'ModController@getAccusationResults');
-
-
 Route::post('/generate_ballot/{gameId}', 'ModController@getNewBallot');
 Route::get('/refresh_ballot/{gameId}/{roundId}', 'ModController@refreshVoteCounts');
 Route::get('/recall_last_ballot/{gameId}', 'ModController@recallLastBallot');
 Route::get('/who_burns/{gameId}/{roundId}', 'ModController@getBurn');
+
+// Routes for Player View
+Route::get('/get_accusable/{game}/{round}', 'PlayerController@getAccusable');
+Route::post('/submit_action/{game_id}/{round_id}', 'PlayerController@submitAction');
